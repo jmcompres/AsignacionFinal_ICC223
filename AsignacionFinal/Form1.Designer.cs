04c3cb2bffe684812e3945900c72e6b7764c27a5
m_com <m_com@hotmail.com>
2025-07-18 17:17:18 -0400

    Agregando menú de pestañas

--- AsignacionFinal/Form1.Designer.cs
+++ AsignacionFinal/Form1.Designer.cs
@@ -28,45 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
+            tabControl1 = new TabControl();
+            tabMenuPrincipal = new TabPage();
+            tabCiudades = new TabPage();
+            tabJugadores = new TabPage();
+            tabEquipos = new TabPage();
+            tabJuegos = new TabPage();
+            tabEstads = new TabPage();
+            tabControl1.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // tabControl1
             // 
-            button1.Location = new Point(380, 129);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            tabControl1.Controls.Add(tabMenuPrincipal);
+            tabControl1.Controls.Add(tabCiudades);
+            tabControl1.Controls.Add(tabJugadores);
+            tabControl1.Controls.Add(tabEquipos);
+            tabControl1.Controls.Add(tabJuegos);
+            tabControl1.Controls.Add(tabEstads);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 450);
+            tabControl1.TabIndex = 0;
             // 
-            // button2
+            // tabMenuPrincipal
             // 
-            button2.Location = new Point(400, 171);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            tabMenuPrincipal.Location = new Point(4, 24);
+            tabMenuPrincipal.Name = "tabMenuPrincipal";
+            tabMenuPrincipal.Padding = new Padding(3);
+            tabMenuPrincipal.Size = new Size(792, 422);
+            tabMenuPrincipal.TabIndex = 0;
+            tabMenuPrincipal.Text = "Menú Principal";
+            tabMenuPrincipal.UseVisualStyleBackColor = true;
+            // 
+            // tabCiudades
+            // 
+            tabCiudades.Location = new Point(4, 24);
+            tabCiudades.Name = "tabCiudades";
+            tabCiudades.Padding = new Padding(3);
+            tabCiudades.Size = new Size(792, 422);
+            tabCiudades.TabIndex = 1;
+            tabCiudades.Text = "Ciudades";
+            tabCiudades.UseVisualStyleBackColor = true;
+            // 
+            // tabJugadores
+            // 
+            tabJugadores.Location = new Point(4, 24);
+            tabJugadores.Name = "tabJugadores";
+            tabJugadores.Padding = new Padding(3);
+            tabJugadores.Size = new Size(792, 422);
+            tabJugadores.TabIndex = 2;
+            tabJugadores.Text = "Jugadores";
+            tabJugadores.UseVisualStyleBackColor = true;
+            // 
+            // tabEquipos
+            // 
+            tabEquipos.Location = new Point(4, 24);
+            tabEquipos.Name = "tabEquipos";
+            tabEquipos.Padding = new Padding(3);
+            tabEquipos.Size = new Size(792, 422);
+            tabEquipos.TabIndex = 3;
+            tabEquipos.Text = "Equipos";
+            tabEquipos.UseVisualStyleBackColor = true;
+            // 
+            // tabJuegos
+            // 
+            tabJuegos.Location = new Point(4, 24);
+            tabJuegos.Name = "tabJuegos";
+            tabJuegos.Padding = new Padding(3);
+            tabJuegos.Size = new Size(792, 422);
+            tabJuegos.TabIndex = 4;
+            tabJuegos.Text = "Juegos";
+            tabJuegos.UseVisualStyleBackColor = true;
+            // 
+            // tabEstads
+            // 
+            tabEstads.Location = new Point(4, 24);
+            tabEstads.Name = "tabEstads";
+            tabEstads.Padding = new Padding(3);
+            tabEstads.Size = new Size(792, 422);
+            tabEstads.TabIndex = 5;
+            tabEstads.Text = "Estadísticas";
+            tabEstads.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(tabControl1);
             Name = "Form1";
             Text = "Form1";
+            tabControl1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
+        private TabControl tabControl1;
+        private TabPage tabMenuPrincipal;
+        private TabPage tabCiudades;
+        private TabPage tabJugadores;
+        private TabPage tabEquipos;
+        private TabPage tabJuegos;
+        private TabPage tabEstads;
     }
 }
