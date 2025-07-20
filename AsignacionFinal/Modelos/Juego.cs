c660bf08a8565907bc08db75054c4e7af68f98df
m_com <m_com@hotmail.com>
2025-07-20 03:10:48 -0400

    CRUD de juegos terminado

---
+++ AsignacionFinal/Modelos/Juego.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsignacionFinal.Modelos
+{
+    public class Juego
+    {
+        public string idJuego {  get; set; }
+        public string descripcion { get; set; }
+        public string idEquipoA { get; set; }
+        public string idEquipoB { get; set; }
+        public DateTime fechaYHora { get; set; }
+    }
+}
--- AsignacionFinal/Visual/Form1.cs
+++ AsignacionFinal/Visual/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using AsignacionFinal.BDD;
+using System.Globalization;
 using System.Windows.Forms;
 
 namespace AsignacionFinal.Visual
@@ -15,6 +16,7 @@ namespace AsignacionFinal.Visual
         {
             loadDataCiudad();
             loadDataEquipo();
+            loadDataJuego();
         }
 
         private void loadDataCiudad()
@@ -24,6 +26,10 @@ namespace AsignacionFinal.Visual
         private void loadDataEquipo()
         {
             dgvEquipos.DataSource = EquipoRepository.GetAll();
+        }
+        private void loadDataJuego()
+        {
+            dgvJuegos.DataSource = JuegoRepository.GetAll();
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -78,6 +84,29 @@ namespace AsignacionFinal.Visual
 
             }
         }
+        private void btnInsertarJuego_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                using var frm = new FormInsertJuego(DateTime.Now);
+                var result = frm.ShowDialog();
+
+                if (result != DialogResult.OK || frm.juego == null)
+                    break; // El usuario canceló o cerró la ventana
+
+                bool exito = JuegoRepository.Insert(frm.juego);
+                loadDataJuego();
+                if (exito)
+                {
+                    MessageBox.Show("Juego insertado correctamente.");
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo insertar el juego. Verifique los datos.");
+                }
+
+            }
+        }
 
 
         //Esto es para editar una ciudad
@@ -117,7 +146,7 @@ namespace AsignacionFinal.Visual
             string idCiudad = EquipoRepository.GetIdCiudad(id);
 
             // Abrir el formulario de edición y pre‑llenar
-            using var frm = new FormInsertEquipo("Editar Equipo",id,nombre,idCiudad);
+            using var frm = new FormInsertEquipo("Editar Equipo", id, nombre, idCiudad);
 
             if (frm.ShowDialog() == DialogResult.OK && frm.equipo != null)
             {
@@ -138,6 +167,41 @@ namespace AsignacionFinal.Visual
                 }
             }
         }
+        private void btnEditarJuego_Click(object sender, EventArgs e)
+        {
+            var row = dgvJuegos.CurrentRow;
+            string id = Convert.ToString(row.Cells["ID"].Value).Trim();
+            string descripcion = row.Cells["Descripcion"].Value.ToString().Trim();
+            string idEqA = JuegoRepository.GetIdEqA(id);
+            string idEqB = JuegoRepository.GetIdEqB(id);
+            DateTime fechaYHora = DateTime.ParseExact(
+                transformarHora(Convert.ToString(row.Cells["Fecha_Y_Hora"].Value).Trim()),
+                "dd/MM/yyyy HH:mm",
+                CultureInfo.InvariantCulture
+            );
+
+            // Abrir el formulario de edición y pre‑llenar
+            using var frm = new FormInsertJuego(fechaYHora,"Editar Juego", id, descripcion, idEqA,idEqB);
+
+            if (frm.ShowDialog() == DialogResult.OK && frm.juego != null)
+            {
+                // Asignar Id y llamar al repositorio
+                var actualizado = frm.juego;
+
+                bool exito = JuegoRepository.Update(actualizado, id);
+                if (exito)
+                {
+                    loadDataJuego();
+                    MessageBox.Show("Juego actualizado correctamente.", "Éxito",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo actualizar el juego.", "Error",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
 
 
         // Eventos de selección de filas (actualizar el enable de botones para borrar y editar)
@@ -150,6 +214,11 @@ namespace AsignacionFinal.Visual
         {
             btnEditarEquipo.Enabled = dgvEquipos.SelectedRows.Count == 1;
             btnEliminarEquipo.Enabled = dgvEquipos.SelectedRows.Count == 1;
+        }
+        private void dgvJuegos_SelectionChanged(object sender, EventArgs e)
+        {
+            btnEditarJuego.Enabled = dgvJuegos.SelectedRows.Count == 1;
+            btnEliminarJuego.Enabled = dgvJuegos.SelectedRows.Count == 1;
         }
 
         private void btnActualizarTabla_Click(object sender, EventArgs e)
@@ -216,6 +285,57 @@ namespace AsignacionFinal.Visual
                                     MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+        }
+        private void btnEliminarJuego_Click(object sender, EventArgs e)
+        {
+            var row = dgvJuegos.CurrentRow;
+            string id = Convert.ToString(row.Cells["ID"].Value).Trim();
+
+            var confirm = MessageBox.Show(
+                "¿Seguro que deseas eliminar al juego seleccionado?",
+                "Confirmar eliminación",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (confirm == DialogResult.Yes)
+            {
+                bool exito = JuegoRepository.Delete(id);
+                if (exito)
+                {
+                    loadDataJuego();
+                    MessageBox.Show("Juego eliminado correctamente.", "Éxito",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo eliminar al juego. Revise sus relaciones.", "Error",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+
+
+        //Funciones de utilidades varias
+        public static string transformarHora(string input)
+        {
+            string[] formatosEntrada = {
+                "d/M/yyyy h:mm:ss tt",
+                "dd/MM/yyyy h:mm:ss tt",
+            };
+
+            var culturaEs = CultureInfo.GetCultureInfo("es-ES");
+
+            DateTime dt = DateTime.ParseExact(
+                input.Trim(),
+                formatosEntrada,
+                culturaEs,
+                DateTimeStyles.None
+            );
+
+            return dt.ToString("dd/MM/yyyy HH:mm");
         }
 
     }
